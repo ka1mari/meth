1ddbdc71f0577c35506ca4ff595749ec6ab445a0
mari <tsujianii@tuta.io>
2023-01-27 18:21:20 +0000

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,156 @@
 #![feature(const_trait_impl)]
+#![feature(platform_intrinsics)]
 #![feature(portable_simd)]
-#![no_std]
+#![feature(repr_simd)]
+#![feature(slice_as_chunks)]
+//#![no_std]
 
-use core::mem;
+use core::{hint, iter, mem, ops, simd::SimdElement};
 
-pub use crate::mixed_slice::MixedSliceExt;
-pub use crate::scalar_slice::ScalarSliceExt;
-pub use crate::simd_slice::SimdSliceExt;
-pub use crate::slice::SliceExt;
+mod intrinsics;
 
-mod mixed_slice;
-mod scalar_slice;
-mod simd_slice;
-mod slice;
+#[derive(Clone, Copy, Debug)]
+#[repr(simd)]
+struct Simd<T, const N: usize>([T; N]);
 
-/// Determine the nearest supported lane size for the provided element.
-pub const fn determine_lanes<T>(mut lanes: usize) -> usize {
-    let size = mem::size_of::<T>();
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Lanes {
+    Zero,
+    One,
+    Two,
+    Four,
+    Eight,
+    Sixteen,
+    ThirtyTwo,
+    SixtyFour,
+}
 
-    // this will be optimized out as `size` is a constant
-    assert!(size <= 64, "up to 512-byte vectors are supported");
+impl<T: Copy, const N: usize> Simd<T, N> {
+    pub const fn from_array(array: [T; N]) -> Self {
+        Self(array)
+    }
 
-    // clamp to the maximum supported vector size
-    lanes = lanes.min(512 / size);
-
-    // check if it's already a valid lane size
-    if lanes.is_power_of_two() {
-        lanes
-    } else {
-        lanes.next_power_of_two() >> 1
+    pub const fn to_array(self) -> [T; N] {
+        self.0
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Lanes {
+    /// Returns the nearest supported lane count.
+    pub const fn nearest<T>(len: usize) -> Self {
+        let size = mem::size_of::<T>();
 
-    #[test]
-    fn lanes() {
-        assert_eq!(determine_lanes::<u8>(0), 0);
-        assert_eq!(determine_lanes::<u8>(1), 1);
+        // this will be optimized out as `size` is a constant
+        assert!(size <= 64, "up to 512-byte vectors are supported");
 
-        assert_eq!(determine_lanes::<u8>(2), 2);
-        assert_eq!(determine_lanes::<u8>(3), 2);
+        // clamp to the maximum supported vector size
+        let len = len.min(64 / size);
 
-        assert_eq!(determine_lanes::<u8>(4), 4);
-        assert_eq!(determine_lanes::<u8>(5), 4);
-        assert_eq!(determine_lanes::<u8>(6), 4);
-        assert_eq!(determine_lanes::<u8>(7), 4);
+        // check if it's already a valid lane size
+        let lanes = if len.is_power_of_two() {
+            len
+        } else {
+            len.next_power_of_two() >> 1
+        };
 
-        assert_eq!(determine_lanes::<u64>(0), 0);
-        assert_eq!(determine_lanes::<u64>(1), 1);
+        // im sure rustc can optimize this
+        match lanes {
+            0 => Self::Zero,
+            1 => Self::One,
+            2 => Self::Two,
+            4 => Self::Four,
+            8 => Self::Eight,
+            16 => Self::Sixteen,
+            32 => Self::ThirtyTwo,
+            64 => Self::SixtyFour,
+            // SAFETY: this cannot go above 64
+            _ => unsafe { hint::unreachable_unchecked() },
+        }
     }
+}
+
+macro_rules! slice_ext {
+    ($($fn:ident, $trait:ident, $intrinsic:ident, $op:tt;)*) => {
+        /// Extension trait for slices of scalars.
+        pub trait SliceExt<T: SimdElement> {
+            $(
+                fn $fn(&mut self, other: &[T])
+                where
+                    T: std::fmt::Debug + ops::$trait;
+            )*
+        }
+
+        impl<T: SimdElement> SliceExt<T> for [T] {
+            $(
+                #[inline]
+                fn $fn(&mut self, other: &[T])
+                where
+                    T: std::fmt::Debug + ops::$trait,
+                {
+                    #[inline]
+                    fn $fn<T, const LANES: usize>(this: &mut [T], other: &[T])
+                    where
+                        T: std::fmt::Debug + SimdElement + ops::$trait,
+                    {
+                        let (this_chunks, this_remainder) = this.as_chunks_mut::<LANES>();
+                        let (other_chunks, other_remainder) = other.as_chunks::<LANES>();
+
+                        for (x, y) in iter::zip(this_chunks.iter_mut(), other_chunks.iter()) {
+                            *x = unsafe {
+                                intrinsics::$intrinsic(
+                                    Simd::from_array(*x),
+                                    Simd::from_array(*y),
+                                ).to_array()
+                            };
+                        }
+
+                        for (x, y) in iter::zip(this_remainder.iter_mut(), other_remainder.iter()) {
+                            *x $op *y;
+                        }
+                    }
+
+                    assert_eq!(self.len(), other.len(), "slices must have the same length");
+
+                    let lanes = Lanes::nearest::<T>(self.len());
+
+                    match lanes {
+                        Lanes::Zero => {}
+                        Lanes::One => $fn::<T, 1>(self, other),
+                        Lanes::Two => $fn::<T, 2>(self, other),
+                        Lanes::Four => $fn::<T, 4>(self, other),
+                        Lanes::Eight => $fn::<T, 8>(self, other),
+                        Lanes::Sixteen => $fn::<T, 16>(self, other),
+                        Lanes::ThirtyTwo => $fn::<T, 32>(self, other),
+                        Lanes::SixtyFour => $fn::<T, 64>(self, other),
+                    };
+                }
+            )*
+        }
+    }
+}
+
+slice_ext! {
+    add, AddAssign, simd_add, +=;
+    div, DivAssign, simd_div, /=;
+    mul, MulAssign, simd_mul, *=;
+    rem, RemAssign, simd_rem, %=;
+    sub, SubAssign, simd_sub, -=;
+
+    bitand, BitAndAssign, simd_and, &=;
+    bitor, BitOrAssign, simd_or, |=;
+    bitxor, BitXorAssign, simd_xor, ^=;
+
+    shl, ShlAssign, simd_shl, <<=;
+    shr, ShrAssign, simd_shr, >>=;
+}
+
+fn main() {
+    use std::array;
+
+    let mut a = array::from_fn::<_, 148, _>(|n| n as u8);
+    let b = array::from_fn::<_, 148, _>(|n| n as u8);
+
+    a.sub(&b);
+
+    println!("{a:?}");
 }
